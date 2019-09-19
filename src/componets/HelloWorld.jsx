da4a4203dbac4e44314912ad6b75d3dc4b7195c3
AcheZeta <ache.mariana@gmail.com>
2019-09-18 19:43:19 -0500

    Change Active with Hooks

--- src/componets/HelloWorld.jsx
+++ src/componets/HelloWorld.jsx
@@ -1,25 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class HelloWorld extends Component {
-  state = {
-    active: true
-  };
-
-  handleClick = () => {
-    this.setState({
-      active: !this.state.active
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Ocultar</button>
-        {this.state.active && <h1>Hola Mundo</h1>}
-      </div>
-    );
-  }
-}
+const HelloWorld = () => {
+  const [active, setActive] = useState(true);
+  const handleClick = () => {
+      setActive(!active)
+  } 
+  return (
+    <div>
+      <button onClick={handleClick}>Ocultar</button>
+      {active && <h1>Hola Mundo</h1>}
+    </div>
+  );
+};
 
 export default HelloWorld;
