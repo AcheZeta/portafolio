dd8cd64020972483abbe7b4d938386d2db8a9364
AcheZeta <ache.mariana@gmail.com>
2019-09-26 15:03:50 -0500

    ADD: Styled components and Css in About component

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -1,30 +1,49 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import Social from './Social'
+import Social from './Social';
+import styled from 'styled-components';
+
+const AboutStyled = styled.div`
+  text-align: center;
+`;
+const AboutAvatar = styled.div`
+  padding: 2em 0 0 0;
+`;
+const AboutImg = styled.img`
+  border-radius: 100%;
+  width: 160px;
+  height: 160px;
+  border: 5px solid #3F0F3F;
+  margin: 0 auto;
+  display: block;
+  box-shadow: 0 0 10px rbga(0,0,0,0.6)
+`;
 
 const About = ({ avatar, name, bio, profession, address }) => (
-    <div className='About'>
-        <div className='About-container'>
-            <div className='About-avatar'>
-                <figure src={ avatar } alt={ name }></figure>
-            </div>
-            <div className='About-name'>
-                <h2>{ name }</h2>
-            </div>
-            <div className='About-profession'>
-                <p>{ profession }</p>
-            </div>
-            <div className='About-description'>
-                <p>{ bio }</p>
-            </div>
-            <div className='About-location'>
-                <p>{ address }</p>
-            </div>
-            <div className='About-social'>
-                <Social Social={Social}/>
-            </div>
-        </div>
+  <AboutStyled>
+    <div className="About-container">
+      <AboutAvatar>
+        <figure>
+          <AboutImg src={avatar} alt={name} />
+        </figure>
+      </AboutAvatar>
+      <div className="About-name">
+        <h2>{name}</h2>
+      </div>
+      <div className="About-profession">
+        <p>{profession}</p>
+      </div>
+      <div className="About-description">
+        <p>{bio}</p>
+      </div>
+      <div className="About-location">
+        <p>{address}</p>
+      </div>
+      <div className="About-social">
+        <Social Social={Social} />
+      </div>
     </div>
-)
-    
+  </AboutStyled>
+);
+
 export default About;
