7124cd250e00d3822a635f35d7d22f7cef664502
AcheZeta <ache.mariana@gmail.com>
2019-10-30 16:28:27 -0600

    Skill Bar animation

--- src/componets/Skill.jsx
+++ src/componets/Skill.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const SkillsContainer = styled.div`
   display: grid;
@@ -17,6 +17,10 @@ const SkillsLine = styled.div`
   border-radius: 50px;
   background: #b29fb2;
 `
+const move = keyframes`
+  0% { background-position 0 0;}
+  100% {background-position: 50px 50px }
+`
 
 const SkillsSpan = styled.span`
   display: block;
@@ -27,7 +31,29 @@ const SkillsSpan = styled.span`
   overflow: hidden;
   position: relative;
   box-shadow: inset 0 2px 9px rgba(255, 255, 255, 0.3),
-    inset 0 -2px 6px rgba(0, 0, 0, 0, 4);
+    inset 0 -2px 6px rgba(0, 0, 0, 0.4);
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-image: linear-gradient(
+      -45deg,
+      rgba(255, 255, 255, 0.2) 25%,
+      transparent 25%,
+      transparent 50%,
+      rgba(255, 255, 255, 0.2) 50%,
+      rgba(255, 255, 255, 0.2) 75%,
+      transparent 75%,
+      transparent
+    );
+    z-index: 1;
+    background-size: 50px 50px;
+    animation: ${move} 4s linear infinite;
+    overflow: hidden;
+  }
 `
 
 const Skill = props => (
