9a114559c7052995e16b555271adf857d44442e0
AcheZeta <ache.mariana@gmail.com>
2019-11-08 19:28:51 -0600

    Fix: typo

--- src/componets/Education.jsx
+++ src/componets/Education.jsx
@@ -7,7 +7,7 @@ import PStyled from '../Styled/PStyled'
 import SpanStyled from '../Styled/SpanStyled'
 
 const Education = props => (
-  <div className='Education'>
+  <div className='<Education'>
     <H2Styled name='<Educación' />
     <div className='Education-container'>
       {props.data.map((edu, index) => (
