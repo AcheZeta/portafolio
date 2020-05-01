5cf6d96ced49f61e24501fad40c5a68fbdfe8e9b
AcheZeta <ache.mariana@gmail.com>
2020-04-30 19:12:26 -0500

    created HR divisor for titles

---
+++ src/Styled/HrStyled.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHR = styled.hr`
+    border: 0;
+    height: 2px;
+    background: #333;
+    width: 50%;
+    text-align: left;
+    margin-left:0;
+    margin-bottom: 1rem;
+`
+
+const HRStyled = () => <StyledHR></StyledHR>
+
+export default HRStyled
