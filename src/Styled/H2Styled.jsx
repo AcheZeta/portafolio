4c24f38b21c913841fab11491e8da5eb0b0c668e
AcheZeta <ache.mariana@gmail.com>
2020-04-30 19:11:59 -0500

    change style component uppercase

--- src/Styled/H2Styled.jsx
+++ src/Styled/H2Styled.jsx
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 
 const StyledH2 = styled.h2`
   font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  letter-spacing: 0.8px;
+  font-weight: 400;
   margin: 1em 0 0 0;
   color: #212121;
+  letter-spacing: 1px;
+  font-size: 2rem;
+  text-transform: uppercase;
 `;
 
-const H2Styled = ({ name }) => <StyledH2>{name}></StyledH2>;
+const H2Styled = ({ name }) => <StyledH2>{name}</StyledH2>;
 
 export default H2Styled;
