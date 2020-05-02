9d47b2a96a40b3ef25d52f418cf36e30b52dfd14
AcheZeta <ache.mariana@gmail.com>
2020-05-01 19:13:18 -0500

    Unificate hard and softskill in a component

--- src/componets/Skill.jsx
+++ src/componets/Skill.jsx
@@ -8,73 +8,49 @@ import HRStyled from '../Styled/HrStyled'
 const SkillsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-column-gap: 2rem;
-  grid-row-gap: 0.5em;
+  grid-template-rows: 1fr;
+  grid-column-gap: 5px;
+  grid-row-gap: 0;
   @media only screen and (max-width: 767px) {
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
   }
 `
 const Skillsh5 = styled.h5`
   margin: 0.5em 0;
 `
-const SkillsLine = styled.div`
-  height: 8px;
-  position: relative;
-  border-radius: 50px;
-  background: #b29fb2;
+const SkillName = styled.h3`
+  color: #212121;
+  font-weight: 300;
+  margin: 0.5em 0;
 `
-const move = keyframes`
-  0% { background-position 0 0;}
-  100% {background-position: 50px 50px }
+const SoftskillDiv = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
 `
-
-const SkillsSpan = styled.span`
-  display: block;
-  height: 100%;
-  width: ${props => props.width};
-  border-radius: 8px;
-  background-color: #3f0f3f;
-  overflow: hidden;
-  position: relative;
-  box-shadow: inset 0 2px 9px rgba(255, 255, 255, 0.3),
-    inset 0 -2px 6px rgba(0, 0, 0, 0.4);
-  &:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-image: linear-gradient(
-      -45deg,
-      rgba(255, 255, 255, 0.2) 25%,
-      transparent 25%,
-      transparent 50%,
-      rgba(255, 255, 255, 0.2) 50%,
-      rgba(255, 255, 255, 0.2) 75%,
-      transparent 75%,
-      transparent
-    );
-    z-index: 1;
-    background-size: 50px 50px;
-    animation: ${move} 4s linear infinite;
-    overflow: hidden;
-  }
+const HardskillDiv = styled.div`
+  grid-area: 1 / 2 / 2 / 3;
 `
 
-const Skill = props => (
+const Skill = ({ hardskills, softskills }) => (
   <div className='Skills'>
     <H2Styled name='Habilidades' />
-    <HRStyled/>
+    <HRStyled />
     <SkillsContainer>
-      {props.data.map((skill, index) => (
-        <div className='Skill-item' key={`skill-${index}`}>
-          <Skillsh5>{skill.name}</Skillsh5>
-          <SkillsLine>
-            <SkillsSpan width={skill.percentage} />
-          </SkillsLine>
-        </div>
-      ))}
+      <SoftskillDiv>
+        <SkillName>Soft Skills</SkillName>
+        {softskills.map((sskill, index) => (
+          <div className='Skill-item' key={`skill-${index}`}>
+            <Skillsh5>{sskill.name}</Skillsh5>
+          </div>
+        ))}
+      </SoftskillDiv>
+      <HardskillDiv>
+        <SkillName>Tech Skills</SkillName>
+        {hardskills.map((hskill, index) => (
+          <div className='Skill-item' key={`skill-${index}`}>
+            <Skillsh5>{hskill.name}</Skillsh5>
+          </div>
+        ))}
+      </HardskillDiv>
     </SkillsContainer>
   </div>
 )
